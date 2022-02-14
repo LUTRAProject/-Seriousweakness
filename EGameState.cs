e191890f58e92f6e6fec5631b13f1036102eb497
LUTRAProject <70827791+LUTRAProject@users.noreply.github.com>
2022-02-14 20:44:37 +0900

    Add files via upload

---
+++ EGameState.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EGameState
+{
+        START = 0,
+        READY = 1,
+        GAME = 2,
+        RESULT = 3
+}
---
+++ TimerManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerManager : MonoBehaviour
+{
+
+    // 時間表示
+    public Text TimerText;
+
+    /// <summary>
+    /// タイマーのテキストの設定
+    /// </summary>
+    public void SetText(int time)
+    {
+
+        this.TimerText.text = "Time : " + time;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
